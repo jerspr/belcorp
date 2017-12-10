c4f5da747dc385c3d72200cfb3a97bfd762f89b9
jesser pariona <jerspr@gmail.com>
2017-12-10 00:41:15 -0500

    üpdate

--- js/main.js
+++ js/main.js
@@ -1,7 +1,30 @@
 var objBel = {
 	init: function(){
-		console.log("Probando..");
+		console.log("Probando...");
+	},
+	/*LLamados de modal de pruebas*/
+	oneModal: function(){
+		$('#oneModal').modal('show');
+	},
+	twoModal: function(){
+		$('#twoModal').modal('show');
+	},
+	threeModal: function(){
+		$('#threeModal').modal('show');
+	},
+	fourModal: function(){
+		$('#fourModal').modal('show');
+	},
+	fiveModal: function(){
+		$('#fiveModal').modal('show');
+	},
+	sixModal: function(){
+		$('#sixModal').modal('show');
+	},
+	sevenModal: function(){
+		$('#sevenModal').modal('show');
 	}
+	/*End*/
 }
 $(document).ready(function(){
 	objBel.init();
